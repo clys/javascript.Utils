cae772abed6e90cd4e5af00ee935962fbac502aa
chenliyusheng <chenliyusheng@yitaosoft.com>
2016-10-26 11:53:33 +0800

    url.getParam 修改

--- utils/utils.js
+++ utils/utils.js
@@ -9,12 +9,7 @@ var utils = {
     },
     url: {
         getParam: function (name) {
-            var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)");
-            var r = window.location.search.substr(1).match(reg);
-            if (r !== null) {
-                return decodeURI(r[2]);
-            }
-            return null;
+            return this.getParamMap()[name] || null;
         },
         getParamKeys: function () {
             return utils.map.keys(this.getParamMap());
